6e5e675fa4d001e4c0c306b2d6c096596a453100
julivert82 <pere.gil@gmail.com>
2016-07-07 23:48:26 +0200

    Last version tested in win10

--- client.py
+++ client.py
@@ -2,10 +2,12 @@
 
 import socket
 
+# open a socket connected to the server port
 sock = socket.socket()
-host = socket.gethostname()
+host = "192.168.1.34" #if the client is not running on the same server change the ip address
 port = 22882
 
+# request for the directory
 sock.connect((host,port))
 sock.send("which files do you have for me?".encode())
 
@@ -13,18 +15,22 @@ sock.send("which files do you have for me?".encode())
 dirs = sock.recv(1024)
 print (dirs.decode())
 
+# select the file and send the choice to the server
 opt = input("Select which file you want to download\n")
 sock.send(opt.encode())
 
+# get the data
 with open('rfile', 'wb') as f:
-        print ('Opening file...')
-        while True:
-                print ('Getting new data...')
-                data = sock.recv(1024)
-                print('data=%s', (data))
-                if not data:
-                        break
-                f.write(data)
+    print ('Opening file...')
+    while True:
+        print ('Getting new data...')
+        data = sock.recv(1024)
+        print('data=%s', (data))
+        if not data:
+                break
+        f.write(data)
+
+# close the connection
 f.close()
 print('Transfer done')
 sock.close()
--- server.py
+++ server.py
@@ -3,21 +3,26 @@
 import socket
 import os, sys
 
+# create socket listening on the port 22882
 port = 22882
 sock = socket.socket()
 host = socket.gethostname()
 sock.bind((host,port))
 sock.listen(5)
 
+# path to the folder where the files are placed
+fpath = "C:\\Users\\Pere\\Desktop\\FileTransfer\\files\\"
+
 print ('Server Ready...')
 
+# loop to get a connection
 while True:
     conn, addr = sock.accept()
     print  ('Connected from', addr)
     data = conn.recv(1024)
     print ('Data received from client', repr(data))
 
-    dirs = os.listdir("C:\\Users\\Pere\\Desktop\\FileTransfer\\files")
+    dirs = os.listdir(fpath)
 
     print (len(dirs))
     ldir = dirs[0]
@@ -28,8 +33,9 @@ while True:
     conn.send(ldir.encode())
 
     print ("File selected")
-    fl = '{0}{1}'.format("C:\\Users\\Pere\\Desktop\\FileTransfer\\files\\",conn.recv(1024).decode())
+    fl = '{0}{1}'.format(fpath,conn.recv(1024).decode())
 
+    # open the selected file and send the data across
     f = open(fl,'rb')
     payload = f.read(1024)
     while(payload):
@@ -38,6 +44,7 @@ while True:
         payload = f.read(1024)
     f.close()
 
+    # end the session
     print ('Transmission done by the server')
     conn.send(b'Fuck you')
     conn.close()
